3946699f54a6478c9584aa001bf0526a71a1d399
brksygmr <brksygmr@outlook.com>
2021-11-19 17:01:30 +0000

    firstandlastdone

--- main.go
+++ main.go
@@ -91,31 +91,36 @@ func printBigChar(chMap *map[byte][]string, inpBSlice []byte) {
 	}
 }
 
-func AsciiArt() {
+func AsciiArt(s string) {
 	// if len(os.Args) != 2 {
 	// 	fmt.Println("Arg amount is not right")
 	// }
+
 	banner := os.Args[2]
 	// read input str
 	var inputStrSlices []string
-	inputStr := os.Args[1]
+
 	// fmt.Println("input: ", inputStr)
-	inputrune := []rune(inputStr)
-	s1 := inputStr
+	inputrune := []rune(s)
+	s1 := s
 	// case when \n at the end of the input
-	if inputrune[len(inputStr)-2] == '\\' && inputrune[len(inputStr)-1] == 'n' {
-		if last := len(s1) - 1; last >= 0 && s1[last] == 'n' {
-			s1 = s1[:last] // remove the last n
+	if len(os.Args[1]) == 4 {
+		if inputrune[len(s)-2] == '\\' && inputrune[len(s)-1] == 'n' {
+			if last := len(s1) - 1; last >= 0 && s1[last] == 'n' {
+				s1 = s1[:last] // remove the last n
+			}
+			s2 := s1
+			if last := len(s2) - 1; last >= 0 && s2[last] == '\\' {
+				s2 = s2[:last] // remove the 2nd last \
+			}
+			// convert s2 (string) to slice of string (with only itself in the slice)
+			inputStrSlices = strings.Split(s2, " ")
+			numberfornewline++ // increment the counter
+		} else {
+			inputStrSlices = strings.Split(s, "\\n") // normal case
 		}
-		s2 := s1
-		if last := len(s2) - 1; last >= 0 && s2[last] == '\\' {
-			s2 = s2[:last] // remove the 2nd last \
-		}
-		// convert s2 (string) to slice of string (with only itself in the slice)
-		inputStrSlices = strings.Split(s2, " ")
-		numberfornewline++ // increment the counter
 	} else {
-		inputStrSlices = strings.Split(inputStr, "\\n") // normal case
+		inputStrSlices = strings.Split(s, "\\n")
 	}
 	for _, inputSlice := range inputStrSlices {
 		// process the str
@@ -146,6 +151,63 @@ func AsciiArt() {
 			charMap[inputBSlice[inp]] = bigChar
 		}
 		// }
+		printBigChar2(&charMap, inputBSlice)
+	}
+}
+
+func AsciiArt2(s string) {
+	// if len(os.Args) != 2 {
+	// 	fmt.Println("Arg amount is not right")
+	// }
+	banner := os.Args[2]
+	// read input str
+	var inputStrSlices []string
+	// inputStr := os.Args[1]
+	// // fmt.Println("input: ", inputStr)
+
+	// case when \n at the end of the input
+
+	inputStrSlices = strings.Split(s, "\\n") // normal case
+
+	for _, inputSlice := range inputStrSlices {
+		// process the str
+		inputBSlice := []byte(inputSlice)
+		// fmt.Println(inputBSlice)
+		charMap := make(map[byte][]string)
+
+		for inp := 0; inp < len(inputBSlice); inp++ {
+			// find the corresponding line num
+			startLine := (int(inputBSlice[inp])-32)*9 + 2
+			// fmt.Println("startLine: ", startLine)
+			// scan the reqired lines in from the txt file
+			fread, err := os.Open(banner + ".txt")
+			checkErr(err)
+			defer fread.Close()
+
+			scanner := bufio.NewScanner(fread)
+
+			scanner.Split(bufio.ScanBytes)
+
+			// scan 8 lines starting from startLine from the txt file
+			bigChar, err := scanChar(fread, startLine)
+			checkErrNoEOF(err)
+			if len(bigChar) != 8 {
+				fmt.Println("Wrong number of lines read")
+			}
+
+			charMap[inputBSlice[inp]] = bigChar
+		}
+		// }
 		printBigChar(&charMap, inputBSlice)
+	}
+}
+
+func printBigChar2(chMap *map[byte][]string, inpBSlice []byte) {
+	for l := 0; l < 8; l++ {
+		chLine := ""
+		for ch := 0; ch < len(inpBSlice); ch++ {
+			chLine += string((*chMap)[inpBSlice[ch]][l])
+			fmt.Println(chLine)
+		}
 	}
 }
--- test/test.go
+++ test/test.go
@@ -1,7 +1,50 @@
 package main
 
-import "asciiart"
+import (
+	"asciiart"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	asciiart.AsciiArt()
+	leninput := len(os.Args[1])
+	if len(os.Args) == 4 {
+		asciiart.AsciiArt(os.Args[1])
+	} else if len(os.Args) == 6 {
+		letters := strings.Split(os.Args[1], "")
+		words := strings.Split(os.Args[1], " ")
+		ourNumber, err := strconv.Atoi(os.Args[5])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+
+		if os.Args[4] == "first" {
+			for i := 0; i < ourNumber; i++ {
+				asciiart.AsciiArt2(letters[i])
+			}
+			for k := ourNumber; k < len(os.Args[1]); k++ {
+				asciiart.AsciiArt(letters[k])
+			}
+		} else if os.Args[4] == "last" {
+			for i := 0; i < (leninput - ourNumber); i++ {
+				asciiart.AsciiArt(letters[i])
+			}
+			for k := (leninput - ourNumber); k < len(os.Args[1]); k++ {
+				asciiart.AsciiArt2(letters[k])
+			}
+		} else if os.Args[4] == "word" {
+			if os.Args[5] != "1" {
+				for i := 0; i < (ourNumber - 1); i++ {
+					asciiart.AsciiArt(words[i])
+				}
+				asciiart.AsciiArt2(words[ourNumber-1])
+				for k := ourNumber - 1; k < len(words); k++ {
+					asciiart.AsciiArt(words[k])
+				}
+			}
+		}
+	}
 }
